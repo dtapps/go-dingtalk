b6d2319df5498a52bf542b507d8d7d23be3eb884
李光春 <gc@dtapp.net>
2021-07-19 16:47:49 +0800

    update *.md

--- example.go
+++ example.go
@@ -7,10 +7,10 @@ import (
 	"testing"
 )
 
-func TestName(t *testing.T) {
+func main(t *testing.T) {
 	bot := dingtalk.DingBot{
-		Secret:      "SEC05342ba24a7eb2e1dbeae61b3df997eb1a97b7cda414566876e983f1db0fec0b",
-		AccessToken: "aad81de7f6b218bb7d085264d4885714c805cc80a460690a0d19db91a05dd174",
+		Secret:      "",
+		AccessToken: "",
 	}
 	msg := message.Message{
 		MsgType: message.TextStr,
